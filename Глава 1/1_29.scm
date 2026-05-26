088586e85679fe5dee9a22db00ba430e122b0dab
ASFominskiy <kel42@mail.ru>
2026-05-26 16:07:48 +0500

    add 1_29 task

---
+++ Глава 1/1_29.scm
@@ -0,0 +1,20 @@
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (simpson-integral f a b n)
+  (define h (/ (- b a) n))
+  
+  (define (y k)
+    (f (+ a (* k h))))
+  
+  (define (term k)
+    (cond ((or (= k 0) (= k n)) (y k))
+          ((even? k) (* 2 (y k)))
+          (else (* 4 (y k)))))
+  
+  (define (next k) (+ k 1))
+  
+  (* (/ h 3) (sum term 0 next n)))
